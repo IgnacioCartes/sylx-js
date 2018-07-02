3cfcb22f29c8be1b6b4e5509f8bc18fdd066e6cd
Ignacio Cartes <ignacio.cartes.w@gmail.com>
2018-07-02 19:28:19 -0400

    Rewrite asset loading logic, adding JSON support

--- src/sylx.asset.js
+++ src/sylx.asset.js
@@ -21,15 +21,89 @@ window.Sylx.Asset = (function (window, Sylx, undefined) {
     // Private methods
 
     /**
-     * Directly loads a new asset
-     * @param   {string} path   - The path to the asset to load
-     * @returns {object} The loaded asset
+     * Loads a new asset into cache
+     * @param {string}  path      Path to the asset
+     * @param {boolean} isPreload Determines whether or not to attach a preload listener to this asset
      */
-    function loadAsset(path) {
-        var newImage = new window.Image();
-        newImage.src = path;
-        Sylx.log("Sylx.Assets: Loaded asset " + path);
-        return newImage;
+    function loadToCache(path, isPreload) {
+
+        // defaults to true
+        isPreload = isPreload || true;
+
+        // get asset type
+        var assetType = getAssetTypeFromPath(path);
+
+        // prepare to load asset depending on type
+        if (assetType === 'image') {
+            // image
+            var newImage = new window.Image();
+            newImage.src = path;
+            if (isPreload) {
+                newImage.onload = onLoadAsset.bind(null, path, newImage);
+            } else {
+                cache[path] = newImage;
+            }
+            return newImage;
+        } else if (assetType === 'json') {
+            // json
+            var xhr = new XMLHttpRequest();
+            // this one line breaks cocoon - so lets comment it out
+            //xhr.overrideMimeType("application/json");
+            xhr.open('GET', path, true);
+            xhr.responseType = 'json';
+
+            // set the onload function
+            // JSON assets will ALWAYS call a callback, regardless of what isPreload is set to
+            xhr.onload = function () {
+                var status = xhr.status;
+                var response = xhr.response;
+                if (status === 200) {
+                    onLoadAsset(path, response);
+                } else {
+                    throw ("JSON asset " + path + " failed to load!", response);
+                }
+            };
+
+            // send request -- have a safe trip, lil request!
+            xhr.send();
+        }
+    }
+
+    /**
+     * Function passed to onload method for assets to track when they're loading
+     * @param {string} path Asset path
+     * @param {object} data Actual asset object
+     */
+    function onLoadAsset(path, data) {
+        cache[path] = data;
+        console.log(path, data);
+        // find asset on preloading array and splice it
+        preloadQueue.splice(preloadQueue.indexOf(path), 1);
+        // if length is 0 then preloading is done
+        if (preloadQueue.length === 0) updateAllLoadedAssets();
+    }
+
+    /**
+     * Updates asset objects when all preloads are finished, fetching from cache object
+     */
+    function updateAllLoadedAssets() {
+        // update asset objects
+        for (var objIndex = 0; objIndex < assetsObjectCollection.length; objIndex++) {
+            var assetObject = assetsObjectCollection[objIndex];
+            // is this asset object referencing something we have in cache?
+            var requestedPath = assetObject.path;
+            if (cache[requestedPath]) {
+                assetObject.data = cache[requestedPath];
+                assetObject.type = getAssetTypeFromPath(requestedPath);
+                assetObject.loaded = true;
+                // once an asset has been loaded, this module can stop caring about it
+                assetsObjectCollection.splice(objIndex, 1);
+                break;
+            }
+        }
+
+        // set property to let the main game module know we're done here
+        $asset._isPreloading = false;
     }
 
     /**
@@ -39,8 +113,6 @@ window.Sylx.Asset = (function (window, Sylx, undefined) {
      * @returns {object} The loaded asset
      */
     function preloadFromUrl(path) {
-        // assuming image now
-
         // first, check if asset is cached already
         if (cache[path]) {
             preloadQueue.splice(preloadQueue.indexOf(path), 1);
@@ -48,48 +120,40 @@ window.Sylx.Asset = (function (window, Sylx, undefined) {
         }
 
         // get new asset
-        var newAsset = new window.Image();
-
-        // set onload
-        newAsset.onload = (function (path) {
-            // find asset on preloading array and splice it
-            preloadQueue.splice(preloadQueue.indexOf(path), 1);
-            // if length is 0 then preloading is done
-            if (preloadQueue.length === 0) $asset._isPreloading = false;
-
-        }).bind(null, path);
-        /*
-        window.setTimeout((function (path) {
-            // find asset on preloading array and splice it
-            preloadQueue.splice(preloadQueue.indexOf(path), 1);
-            // if length is 0 then preloading is done
-            if (preloadQueue.length === 0) $asset._isPreloading = false;
-
-        }).bind(null, path), Math.random() * 1500); // to simulate a longer load time
-        */
-
-        newAsset.src = path;
-        Sylx.log("Sylx.Assets: Loaded asset " + path);
-        cache[path] = newAsset;
+        var newAsset = loadToCache(path, true);
         return newAsset;
+    }
+
+    /**
+     * Determines asset type based on its extension
+     * @param   {string} path   Asset path
+     * @returns {string} Asset type as a string
+     */
+    function getAssetTypeFromPath(path) {
+        // get file extension
+        var a = path.split("."),
+            ext;
+        if (a.length === 1 || (a[0] === "" && a.length === 2)) {
+            ext = "";
+        } else {
+            ext = a.pop().toLowerCase();
+        }
+
+        // determine extension
+        switch (ext) {
+            case "png":
+            case "gif":
+            case "jpg":
+                return "image";
+            case "json":
+                return "json";
+        }
+
     }
 
 
     // Exportable object
     var $asset = {
-        /**
-         * Gets a new asset, either from cache or by directly loading it
-         * @param   {string} path   Path to the asset to get
-         * @returns {object} The loaded or cached asset
-         */
-        get: function (path) {
-            // actually load new asset
-            // a relic from the past
-            if (cache[path])
-                return cache[path];
-            else
-                return loadAsset(path);
-        },
         /**
          * Creates a new asset object that will hold an asset in the future
          * @param   {path}   path  Path to the asset to load
@@ -107,6 +171,7 @@ window.Sylx.Asset = (function (window, Sylx, undefined) {
             if (cache[path]) {
                 // "load" inmediately if true
                 newAsset.data = cache[path];
+                newAsset.type = getAssetTypeFromPath(path);
                 newAsset.loaded = true;
             } else {
                 assetsObjectCollection.push(newAsset);
@@ -144,22 +209,10 @@ window.Sylx.Asset = (function (window, Sylx, undefined) {
             this._isPreloading = true;
 
             // iterate through queue
-            for (var index = 0; index < preloadQueue.length; index++) {
+            for (var index = 0; index < preloadQueue.length; index++)
                 // run preload logic
-                var path = preloadQueue[index].path;
-                var data = preloadFromUrl(path);
-                // find if any asset object requires this particular asset
-                for (var objIndex = 0; objIndex < assetsObjectCollection.length; objIndex++) {
-                    var assetObject = assetsObjectCollection[objIndex];
-                    if ((assetObject.path === path) && (assetObject.loaded === false)) {
-                        assetObject.data = data;
-                        assetObject.loaded = true;
-                        // once an asset has been loaded, this module can stop caring about it
-                        assetsObjectCollection.splice(objIndex, 1);
-                        break;
-                    }
-                }
-            }
+                preloadFromUrl(preloadQueue[index].path);
+
         },
         /**
          * Gets the preloading progress
